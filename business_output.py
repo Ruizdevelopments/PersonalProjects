cc8d82e6f4494fab87f40fc0d8021217803db264
Ruben Ruiz <ruben__ruiz@hotmail.com>
2025-09-15 12:51:06 -0700

    Delete business_output.py

--- business_output.py
+++
@@ -1,8 +0,0 @@
-Scheduler - Client: Ruben, Date: 2002-02-22, Time: 03:33
-Inventory - Product: Bracalet, Stock: 5, Sold: 2, Remaining: 3
---- INVOICE ---
-Client: Ruben
-Item: Gas
-Total Due: $50.00
-
-
